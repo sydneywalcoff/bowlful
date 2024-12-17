705bef7a0400b5ac7d6ef7bb47ddbbf894ab4a62
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 16:05:39 -0800

    init User file in models and connected sequelize to the start of the server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT, () => {
-    console.log('API Server listening on port: '+ PORT);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('API Server listening on port: ' + PORT));
 })
