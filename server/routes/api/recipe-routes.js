06fa2e88bee555db027bb1baf2bcd7f743ebad2e
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 18:52:28 -0800

    plan out recipe routes

--- server/routes/api/recipe-routes.js
+++ server/routes/api/recipe-routes.js
@@ -1,1 +1,13 @@
+const router = require('express').Router();
 
+const { Recipe } = require('../../models');
+
+// Get all Recipes
+
+// Get Recipe by ID
+
+// Add new Recipe
+
+// Update Recipe
+
+// Delete Recipe
