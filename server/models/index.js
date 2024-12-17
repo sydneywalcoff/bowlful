ccf3cad2f88b3ebd08a6c791bd3ed12ca24fce3d
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 21:04:34 -0800

    add model associations

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,28 @@ const User = require("./User");
 const Recipe = require("./Recipe");
 const Note = require("./Note");
 
+User.hasMany(Recipe, {
+    foreignKey: 'user_id'
+});
+
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Recipe.hasMany(Note, {
+    foreignKey: 'recipe_id'
+});
+
+Note.belongsTo(Recipe, {
+    foreignKey: 'recipe_id'
+});
+
+User.hasMany(Note, {
+    foreignKey: 'user_id'
+});
+
+Note.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = { User, Recipe, Note };
