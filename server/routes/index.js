9579735e22e60a0c057d533cef9834cd9b9d1895
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 16:09:48 -0800

    create routes folder to organize routes, add to server, and add inital error route

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const apiRoutes = require('./api');
+
+router.use((req, res) => {
+    res.send("<h1>Wrong Route</h1>")
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 
+const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('API Server listening on port: ' + PORT));
