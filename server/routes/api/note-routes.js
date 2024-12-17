983e57246b9b937ceb7a39dda2099b6a6ce57c43
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 18:52:52 -0800

    plan out note routes

--- server/routes/api/note-routes.js
+++ server/routes/api/note-routes.js
@@ -1,1 +1,13 @@
+const router = require('express').Router();
 
+const { Note } = require('../../models');
+
+// Get all Notes
+
+// Get Note by ID
+
+// Add new Note
+
+// Update Note
+
+// Delete Note
