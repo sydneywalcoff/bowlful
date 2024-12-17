ad2a0a1e3886a434949357dbcbf2d666d0cf5cc7
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-12-16 19:12:38 -0800

    added GET ALL recipe route

--- server/routes/api/recipe-routes.js
+++ server/routes/api/recipe-routes.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 
-const { Recipe } = require('../../models');
+const { Recipe, Note, User } = require('../../models');
 
 // Get all Recipes
+router.get('/', (req, res) => {
+    Recipe.findAll({
+        attributes: ['id', 'name', 'ingredients', 'instructions', 'favorite', 'url', 'have_tried', 'user_id', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Note,
+                attributes: ['id', 'text', 'created_at', 'user_id'],
+                include: [{
+                    model: User
+                }]
+            },
+            {
+                model: User,
+                attributes: ['id', 'username']
+            }
+        ]
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
 
 // Get Recipe by ID
 
